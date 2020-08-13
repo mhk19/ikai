015b925875cae91c6175435f840cacbf44473ada
shreyaa-sharmaa <shreyasharma.ss305@gmail.com>
2020-08-13 23:40:26 +0530

    network permission requested

--- src/offShare/model/style.js
+++ src/offShare/model/style.js
@@ -1,26 +1,26 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 let style = StyleSheet.create({
-    header:{
-        backgroundColor:'#212121',
-        flexDirection:'row',
-        elevation:0.5,
-        alignItems:'flex-end'
+    header: {
+        backgroundColor: '#212121',
+        flexDirection: 'row',
+        elevation: 0.5,
+        alignItems: 'flex-end'
     },
-    headerText:{
-        color:"#fff",
-        fontSize:18,
-        fontWeight:'bold'
+    headerText: {
+        color: "#fff",
+        fontSize: 18,
+        fontWeight: 'bold'
     },
-    text:{
-        fontSize:16,
-        fontWeight:'bold',
-        marginBottom:2,
+    text: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 2,
     },
-    buttonText:{
-        fontSize:18,
-        fontWeight:'bold',
-        marginBottom:2,
+    buttonText: {
+        fontSize: 18,
+        fontWeight: 'bold',
+        marginBottom: 2,
     }
 })
 
--- src/offShare/utils/NearbyDevices.js
+++ src/offShare/utils/NearbyDevices.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -12,39 +12,39 @@ import {
 import Modal from 'react-native-modal';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import Button from 'apsl-react-native-button';
-import style from '../assets/styles/style';
+import style from '../model/style';
 import { WifiWizard } from 'react-native-wifi-and-hotspot-wizard';
 import Toast from 'react-native-simple-toast';
 import { FlatList } from 'react-native-gesture-handler';
 
-let NearbyDevices = () =>{
-    let [nearbyNetworksList,setNearbyNetworks] = useState([]);
-   
+let NearbyDevices = () => {
+  let [nearbyNetworksList, setNearbyNetworks] = useState([]);
 
-        WifiWizard.getNearbyNetworks().then(devices=>{
-            // List Of Devices [Containing Only SSID]
-            let devices_list = devices.map(device=>{
-                return device.SSID;
-            })
-            setNearbyNetworks(devices_list);
-            }).catch(err=>{
-            console.log(err)
-            })
-   
-    return (
-        <View>
-        <Text style={style.text}>Nearby Networks </Text>
-        {nearbyNetworksList.length>=1?
-        <FlatList 
-        data={nearbyNetworksList}
-        renderItem={({item,index})=>(
-          <View key={index} style={{flexDirection:'row',marginTop:20}}>
-            <Icon name="wifi" solid={true} color="#212121" size={14}></Icon><Text style={{color:'#5A4C98',fontWeight:'bold',fontSize:16,textAlignVertical:'bottom'}}> {item} </Text>
-          </View>
-        )}
-        ></FlatList>:<Text style={{marginBottom:15}}>Scanning for networks...</Text>}
-        </View>
-    )
+
+  WifiWizard.getNearbyNetworks().then(devices => {
+    // List Of Devices [Containing Only SSID]
+    let devices_list = devices.map(device => {
+      return device.SSID;
+    })
+    setNearbyNetworks(devices_list);
+  }).catch(err => {
+    console.log(err)
+  })
+
+  return (
+    <View>
+      <Text style={style.text}>Nearby Networks </Text>
+      {nearbyNetworksList.length >= 1 ?
+        <FlatList
+          data={nearbyNetworksList}
+          renderItem={({ item, index }) => (
+            <View key={index} style={{ flexDirection: 'row', marginTop: 20 }}>
+              <Icon name="wifi" solid={true} color="#212121" size={14}></Icon><Text style={{ color: '#5A4C98', fontWeight: 'bold', fontSize: 16, textAlignVertical: 'bottom' }}> {item} </Text>
+            </View>
+          )}
+        ></FlatList> : <Text style={{ marginBottom: 15 }}>Scanning for networks...</Text>}
+    </View>
+  )
 }
 
 export default NearbyDevices;
